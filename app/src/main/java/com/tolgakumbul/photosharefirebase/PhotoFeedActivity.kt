f35487bd0e5738d8a868e1409662a6199621b2a3
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2022-09-25 16:41:50 +0300

    added menu for login/add photo, and new activity

--- app/src/main/java/com/tolgakumbul/photosharefirebase/PhotoFeedActivity.kt
+++ app/src/main/java/com/tolgakumbul/photosharefirebase/PhotoFeedActivity.kt
@@ -1,11 +1,44 @@
 package com.tolgakumbul.photosharefirebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class PhotoFeedActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_feed)
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //To connect menu with the activity
+        val menuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout){
+            //Logout from Firebase
+            auth.signOut()
+            //Back to the login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else if(item.itemId == R.id.add_photo){
+            //Checkout to the add photo activity
+            val intent = Intent(this, AddPhotoActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
