f0cde16c06bcb61d4b4956ed4bc680d479a27a4f
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2022-09-25 18:18:04 +0300

    Gallery permission added

--- app/src/main/java/com/tolgakumbul/photosharefirebase/AddPhotoActivity.kt
+++ app/src/main/java/com/tolgakumbul/photosharefirebase/AddPhotoActivity.kt
@@ -1,11 +1,87 @@
 package com.tolgakumbul.photosharefirebase
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class AddPhotoActivity : AppCompatActivity() {
+
+    var pickedImage: Uri? = null
+    var pickedBitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
     }
+
+    fun pickPhoto(view: View) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        } else {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, 2)
+        }
+    }
+
+    fun addPhoto(view: View) {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galleryIntent =
+                    android.content.Intent(
+                        android.content.Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                startActivityForResult(galleryIntent, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            pickedImage = data.data
+            if (pickedImage != null) {
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                if (Build.VERSION.SDK_INT >= 28) {
+                    val source = ImageDecoder.createSource(this.contentResolver, pickedImage!!)
+                    pickedBitmap = ImageDecoder.decodeBitmap(source)
+                    imageView.setImageBitmap(pickedBitmap)
+                } else {
+                    pickedBitmap =
+                        MediaStore.Images.Media.getBitmap(this.contentResolver, pickedImage)
+                    imageView.setImageBitmap(pickedBitmap)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
