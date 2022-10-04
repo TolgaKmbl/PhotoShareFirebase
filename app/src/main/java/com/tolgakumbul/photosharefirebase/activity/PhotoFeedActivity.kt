fab6b6351078b29a2be5f5ff80dc4df3cf9307a7
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2022-10-04 22:19:36 +0300

    finalize with recycler adapter

--- app/src/main/java/com/tolgakumbul/photosharefirebase/activity/PhotoFeedActivity.kt
+++ app/src/main/java/com/tolgakumbul/photosharefirebase/activity/PhotoFeedActivity.kt
@@ -6,18 +6,22 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 import com.tolgakumbul.photosharefirebase.R
+import com.tolgakumbul.photosharefirebase.adapter.PhotoFeedRecyclerAdapter
 import com.tolgakumbul.photosharefirebase.model.Post
 
 class PhotoFeedActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseFirestore
+    private lateinit var recyclerViewAdapter : PhotoFeedRecyclerAdapter
 
     var postList = ArrayList<Post>()
 
@@ -26,6 +30,14 @@ class PhotoFeedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo_feed)
         auth = Firebase.auth
         database = FirebaseFirestore.getInstance()
+
+        getData()
+
+        val layoutManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        recyclerViewAdapter = PhotoFeedRecyclerAdapter(postList)
+        recyclerView.adapter = recyclerViewAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -70,6 +82,8 @@ class PhotoFeedActivity : AppCompatActivity() {
                         val downloadedPost = Post(currentUserMail, userComment, imageUrl)
                         postList.add(downloadedPost)
                     }
+                    // To refresh the recyclerViewAdapter
+                    recyclerViewAdapter.notifyDataSetChanged()
                 }
             }
         }
--- app/src/main/java/com/tolgakumbul/photosharefirebase/adapter/PhotoFeedRecyclerAdapter.kt
+++ app/src/main/java/com/tolgakumbul/photosharefirebase/adapter/PhotoFeedRecyclerAdapter.kt
@@ -1,4 +1,38 @@
 package com.tolgakumbul.photosharefirebase.adapter
 
-class PhotoFeedRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.tolgakumbul.photosharefirebase.R
+import com.tolgakumbul.photosharefirebase.model.Post
+
+class PhotoFeedRecyclerAdapter(val postList: ArrayList<Post>) :
+    RecyclerView.Adapter<PhotoFeedRecyclerAdapter.PostHolder>() {
+
+    class PostHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_row, parent, false)
+        return PostHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.recycler_row_user_email).text =
+            postList[position].userMail
+        holder.itemView.findViewById<TextView>(R.id.recycler_row_user_comment).text =
+            postList[position].userComment
+        Picasso.get().load(postList[position].imageUrl)
+            .into(holder.itemView.findViewById<ImageView>(R.id.recycler_row_image_view))
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
 }
